38e58eaef6a433b94da367acb1bf6ee8208e2b25
andreystarenky <47681471+andreystarenky@users.noreply.github.com>
2021-09-28 22:04:00 -0400

    Lab 2 work

---
+++ Assignments/ASSIGNMENT 1/Assign1.py
@@ -0,0 +1,60 @@
+# Andrey Starenky
+# Assign 1
+# September 28, 2021
+# CS1026A
+
+# Constant Prices
+OFF_PEAK_PRICE = 0.085
+ON_PEAK_PRICE = 0.176
+MID_PEAK_PRICE = 0.119
+
+# Constant Discounts
+TOTAL_USAGE_DISCOUNT = 0.04
+ON_PEAK_DISCOUNT = 0.05
+SENIOR_DISCOUNT = 0.11
+
+# Constant Tax
+TAX = 0.13
+
+while True: # loop forever until break; is called
+    # get input for off peak time
+    offPeakInput = float(input("Enter kwh during Off Peak period: "))
+
+    if offPeakInput == 0: # if off peak input is 0 then break out of the loop
+        break # break out of the loop
+
+    # Get on peak input
+    onPeakInput = float(input("Enter kwh during On Peak period: "))
+    # Get mid peak input
+    midPeakInput = float(input("Enter kwh during Mid Peak period: "))
+    # Get is senior input
+    isSeniorInput = input("Is owner senior? (Y,y,N,n): ")
+
+    # Calculate Prices Prior to Discounts
+    offPeakCost = offPeakInput * OFF_PEAK_PRICE
+    onPeakCost = onPeakInput * ON_PEAK_PRICE
+    midPeakCost = midPeakInput * MID_PEAK_PRICE
+
+    # Calculate Discounts
+    # Total Usage Discount
+
+
+    # On Peak Discount
+    if onPeakInput<150 and offPeakInput<400:
+        onPeakCost *= (1-ON_PEAK_DISCOUNT)
+
+    # Total Cost
+    totalCost = offPeakCost + onPeakCost + midPeakCost
+
+    if offPeakInput+onPeakInput+midPeakInput<400:
+        totalCost *= (1-TOTAL_USAGE_DISCOUNT)
+
+    # Senior Discount
+    if isSeniorInput == 'Y' or isSeniorInput == 'y':
+        totalCost *= (1-SENIOR_DISCOUNT)
+
+    # TAX
+    totalCost *= (1+TAX)
+
+    # Output result
+    print("Electricity cost: $" + "%.2f" % (totalCost))
---
+++ LABS/LAB 2/labrun2.py
@@ -0,0 +1,24 @@
+'''
+clientAge = float(input("Please enter your age in years: "))
+if(clientAge>=9):
+    clientHeight = float(input("Please enter your height in cm: "))
+    if(clientHeight>=130):
+        print("You may go on the ride")
+    else:
+        print("Sorry you are too short to go on the ride")
+else:
+    print("Sorry, you are too young for this ride")
+'''
+
+IDEAL_CREDIT_SCORE = 720
+userScore = int(input("Please enter your credit score: "))
+housePrice = int(input("Please enter the price of the house: "))
+
+if userScore >= IDEAL_CREDIT_SCORE: # switched => to >=
+    downPayment = 0.1 * housePrice
+elif userScore < IDEAL_CREDIT_SCORE and userScore > 600: # switched else if to elif, and "600" to 600
+    downPayment = 0.2 * housePrice
+else:
+    downPayment = 0.3 * housePrice # was not indented
+
+print("Your down payment is: ${}".format(downPayment))
