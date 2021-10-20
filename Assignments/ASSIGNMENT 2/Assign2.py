0eecbdc0c9fe6815fe7ff2d2a08756116c5ed748
andreystarenky <47681471+andreystarenky@users.noreply.github.com>
2021-10-20 14:30:52 -0400

    Assignment 2

---
+++ Assignments/ASSIGNMENT 2/Assign2.py
@@ -0,0 +1,102 @@
+# Andrey Starenky
+# astarenk 251214306
+# Assign 1
+# CS1026A
+# October 06, 2021
+# This program checks codes inputted by the user to determine the algorithms used by checking potential options against the check code
+
+# Import the code_check.py file
+import code_check
+
+# Initialize code lists
+basicList = []
+positionList = []
+upcList = []
+noneList = []
+
+# Loop until broken out by input of 0
+while True:
+    # Get user input
+    userInput = input("Please enter code (digits only) (enter 0 to quit) ")
+
+    # Check if input is 0
+    if userInput == "0":
+        break
+
+    # Make a default none variable - when the code gets added to a list, this will become false
+    none = True
+
+    # Check for basic code
+    if code_check.checkBasic(userInput):
+        basicList.append(userInput) # Add to list
+        print("-- code: " + userInput + " valid Basic code.")
+        none = False # None is false, as one code matched
+
+    # Check for position code
+    if code_check.checkPosition(userInput) == True:
+        positionList.append(userInput) # Add to list
+        print("-- code: " + userInput + " valid Position code.")
+        none = False # None is false, as one code matched
+
+    # Check for UPC code
+    if code_check.checkUPC(userInput) == True:
+        upcList.append(userInput) # Add to list
+        print("-- code: " + userInput + " valid UPC code.")
+        none = False # None is false, as one code matched
+
+    # If none is still true, none of the codes matched
+    if none:
+        noneList.append(userInput) # Add to none list
+        print("-- code: " + userInput + " not Basic, Position or UPC code.")
+
+#Summary
+print("Summary")
+# Basic Codes
+print("Basic: ", end = "")
+# Print out each code
+for x in range(0,len(basicList)):
+    # IF LAST ITEM, DON'T PRINT COMMA
+    if (x == len(basicList)-1):
+        print(basicList[x], end="")
+    else: # ELSE PRINT COMMA
+        print(basicList[x], end=", ")
+# If no codes, print none
+if(len(basicList) == 0):
+    print("None")
+else: print()
+
+# Position Codes
+print("Position: ", end = "")
+# Print out each code
+for x in range(0,len(positionList)):
+    # IF LAST ITEM, DON'T PRINT COMMA
+    if (x == len(positionList)-1):
+        print(positionList[x], end="")
+    else: # ELSE PRINT COMMA
+        print(positionList[x], end=", ")
+# If no codes, print none
+if(len(positionList) == 0):
+    print("None")
+else: print()
+
+# UPC Codes
+print("UPC: ", end = "")
+for x in range(0,len(upcList)):
+    # IF LAST ITEM, DON'T PRINT COMMA
+    if (x == len(upcList)-1):
+        print(upcList[x], end="")
+    else: # ELSE PRINT COMMA
+        print(upcList[x], end=", ")
+# If no codes, print none
+if(len(upcList) == 0):
+    print("None")
+else: print()
+
+# None Codes
+print("None: ", end = "")
+for x in noneList:
+    print(x, end=" ")
+# If no codes, print none
+if(len(noneList) == 0):
+    print("None")
+else: print()
---
+++ Assignments/ASSIGNMENT 2/code_check.py
@@ -0,0 +1,36 @@
+# Check for basic code
+def checkBasic(checkString):
+    sum = 0 # Default sum is 0
+    for x in checkString[0:-1]: # Loop through string EXCEPT last character
+        sum += int(x) # Add digit to sum
+    checkDigit = sum % 10 # Get mod 10
+    if checkDigit == int(checkString[len(checkString) - 1]):
+        return True # Return True if the check digit matches the given input
+    return False
+
+# Check for position code
+def checkPosition(checkString):
+    sum = 0 # Default sum is 0
+    for x in range(0,len(checkString)-1):  # Loop through string EXCEPT last character
+        sum += (int(checkString[x]) * (x+1)) # Add the digit * position to the sum
+    checkDigit = sum % 10 # Get mod 10
+    if checkDigit == int(checkString[len(checkString) - 1]):
+        return True # Return True if the check digit matches the given input
+    return False
+
+# Check for UPC code
+def checkUPC(checkString):
+    sum = 0 # Default sum is 0
+    for x in range(0, len(checkString) - 1):  # Loop through string EXCEPT last character
+        if (x+1)%2 == 1: # Check if odd position
+            sum += (int(checkString[x]) * 3) # Add digit multiplied by 3 to sum
+        else: # Else even position
+            sum += int(checkString[x]) # Add digit to sum
+    if (sum % 10 != 0):
+        checkDigit = 10 - (sum % 10) # Get 10 - mod 10 if mod 10 is not 0
+    else:
+        checkDigit = 0
+
+    if checkDigit == int(checkString[len(checkString) - 1]):
+        return True # Return True if the check digit matches the given input
+    return False
